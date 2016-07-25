f11d92a2dd0265b2d90c1e3f4fc50c11bb4e5c9b
Manish Malik <manishmalikkvs@gmail.com>
2016-07-25 09:51:59 -0700

    Adding body parser and dummy POST endpoint

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 var connector = require('./connector');
 var express = require('express');
+var bodyParser = require('body-parser')
 
 //calling modsec connector's constructor
 var modsec = new connector(null, './basic_rules.conf');
 
 var app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
 app.use(modsec.init());
 
 app.get('/', function(req, res) {
 	// console.log(listener.address());
 	res.send('GET request to homepage');
+});
+
+app.post('/test', function(req,res){
+	res.send('POST request to /test');
 });
 
 listener = app.listen(3000, function() {
